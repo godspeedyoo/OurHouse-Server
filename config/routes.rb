ecc92a1f7b8b0cd5074d31c966d27d5994fe0785
Charles Kim & godspeedyoo <pair+ckim0706.godspeedyoo@devbootcamp.com>
2015-02-13 21:43:15 -0800

    added routes, yay

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+
+  resources :users do
+  resources :houses do
+    resources :payments
+    resources :messages
+  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
