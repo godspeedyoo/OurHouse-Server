e9c4d9d448e53fec3b1bf73089cb8e3466e22e2e
Anthony Ng & Charles Kim <pair+anthony-ng.ckim0706@devbootcamp.com>
2015-02-14 21:11:48 -0800

    created payment controller routes; updated RDoc for payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,23 +1,23 @@
-class MessagesController < ApplicationController
+class PaymentsController < ApplicationController
 
   def index
-
+    payments = Payment.all #change to current user in production
+    render json: payments
   end
 
   def show
-
+    payment = Payment.find(params[:id])
+    render json: payment
   end
 
   def create
+    payment = Payment.new(params[:payment])
 
+    if payment.save
+      render json: payment
+    else
+      render json: { payment: 'Payment not created' }, status: 403
+    end
   end
-
-  # def update
-
-  # end
-
-  # def destroy
-
-  # end
 
 end
