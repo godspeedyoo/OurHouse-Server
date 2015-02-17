8e9d5bed28c5eb95419ab50a46da7114a7be3d5f
Joseph Won <godspeedyoo@gmail.com>
2015-02-17 12:06:58 -0800

    replced hardcoded user with current_user.id. added clarifying comments on message_users.update

--- app/controllers/messages_users_controller.rb
+++ app/controllers/messages_users_controller.rb
@@ -1,8 +1,8 @@
 class MessagesUsersController < SecuredController
 
   def update
-    messages_users = MessagesUser.find_by(id: params[:id], user_id: 1) #replace 1 with current_user.id in production
-    messages_users.update(params) #figure this shit out
+    messages_users = MessagesUser.find_by(id: params[:id], user_id: current_user.id)
+    messages_users.update(params) #This is incomplete, should update this to change a specific users view (i.e. message read, deleted...etc)
   end
 
 end
