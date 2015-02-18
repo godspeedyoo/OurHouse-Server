049ad24795c66fa78235be8ed06a720099f4055d
Brian Vogelgesang <kida001@gmail.com>
2015-02-17 20:08:56 -0800

    done

--- app/controllers/message_helper.rb
+++ app/controllers/message_helper.rb
@@ -8,6 +8,8 @@ module MessagesViewsHelper
       message.messages_users.each do |view|
         parsed_view = JSON.parse(view.to_json)
         parsed_message["type"] = Message.find(parsed_message["id"]).type
+        parsed_message["fname"] = User.find(parsed_message["user_id"]).first_name
+        parsed_message["image"] = User.find(parsed_message["user_id"]).image
         parsed_message["view"] = parsed_view
         all_messages.push(parsed_message) if view.user.id == user.id
       end
