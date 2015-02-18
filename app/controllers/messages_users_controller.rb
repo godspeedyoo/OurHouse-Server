ea22ebff2421bcada644bf9a53848011bf2d2140
Brian Vogelgesang <kida001@gmail.com>
2015-02-17 22:02:15 -0800

    fuck it

--- app/controllers/messages_users_controller.rb
+++ app/controllers/messages_users_controller.rb
@@ -1,8 +1,20 @@
 class MessagesUsersController < SecuredController
 
   def update
+    debugger
     messages_users = MessagesUser.find_by(id: params[:id], user_id: current_user.id)
-    messages_users.update(params) #This is incomplete, should update this to change a specific users view (i.e. message read, deleted...etc)
+    messages_users.update(view_params)
+  end
+
+  private
+  def view_params
+    params.require(:view).permit(:removed, :read, :message_id)
   end
 
 end
+
+
+__END__
+message id: 1
+user viewing: 2
+message view id: 2
